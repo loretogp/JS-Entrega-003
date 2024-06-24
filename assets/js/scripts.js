530f3ffb99120a08d48321f48d7c6be419dcdc66
loretogp <loretogp4@gmail.com>
2024-06-24 18:07:31 -0400

    modifica forma del cuadrado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
 
 
-var pintar = document.querySelector('#ele1');
+var pintar = document.querySelector('#cuadrado');
 pintar.addEventListener('click', function(){ //aquí se crea una función anónima por requerimiento del ejercicio
     if(pintar.style.backgroundColor!=='green'){
         pintar.style.backgroundColor='green';
